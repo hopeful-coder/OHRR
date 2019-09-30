d4ab8138458886db94d9e77f6d278915c14754e7
hopeful-coder <mschepps21@gmail.com>
2019-09-29 17:17:12 -0700

    Initially copy and pasted old code into github

--- first_code.R
+++ first_code.R
@@ -1,1 +1,60 @@
 #This is a second upload into Github from R
+
+#Load data in
+library(haven)
+library(kableExtra)
+library(plyr)
+library(readr)
+library(dplyr)
+OHRR_All_Flattened <- read_sav("C:/Users/Mitchell Schepps/Desktop/nena/OHRR_All_Flattened.sav")
+# OHRR_All_Flattened2 <- read_sav("C:/Users/Mitchell Schepps/Desktop/nena/OHRR_All_Flattened052919.sav")
+group              <- read.csv("C:/Users/Mitchell Schepps/Desktop/nena/group.csv")
+labels             <- read.csv("C:/Users/Mitchell Schepps/Desktop/nena/labels.csv", header = F)
+
+list1              <- read.csv("C:/Users/Mitchell Schepps/Desktop/nena/list1.csv")
+list2              <- read.csv("C:/Users/Mitchell Schepps/Desktop/nena/list2.csv")
+list3              <- read.csv("C:/Users/Mitchell Schepps/Desktop/nena/list3.csv")
+list4              <- read.csv("C:/Users/Mitchell Schepps/Desktop/nena/list4.csv")
+list5              <- read.csv("C:/Users/Mitchell Schepps/Desktop/nena/list5.csv")
+recid <- read_sav("C:/Users/Mitchell Schepps/Desktop/nena/recid_data.sav")
+recid %>%
+  mutate_all(as.character)
+recid <- data.frame(recid)
+names(recid)[8:ncol(recid)] <- recid[1, c(8:ncol(recid))]
+recid <- recid[6:nrow(recid),]
+recid$Gender <- tolower(recid$Gender)
+
+
+full_id = read.csv("C:/Users/Mitchell Schepps/Desktop/nena/full_id.csv")
+names(full_id)[5] = 'UCLACLIENTID'
+names(full_id)[1] = 'BookingNumber'
+full_id = full_id[6:nrow(full_id), ]
+recid = recid[order(recid$BookingNumber), ]
+full_id = full_id[order(full_id$BookingNumber), ]
+
+recid$UCLACLIENTID = full_id$UCLACLIENTID
+
+table(recid$Gender, recid$GROUPASSIGNED)
+withdrawal <- c(2024, 2029, 2034, 2040, 2045, 1058)
+withd <- which(recid$UCLACLIENTID %in% withdrawal)
+recid <- recid[-withd,]
+
+names(labels)[1] <- 'Variable'
+names(labels)[5] <- 'Name'
+
+names(group)[1] <- 'ID1'
+data <- merge(OHRR_All_Flattened, group, by = 'ID1')
+
+withdrawal <- c(2024, 2029, 2034, 2040, 2045, 1058)
+
+data <- data[!data$ID1 %in% withdrawal,]
+data$age <- 2019 - data$QDS_BL_DEM1Y
+
+recid = recid[order(recid$UCLACLIENTID), ]
+data = data[order(data$ID1), ]
+
+recid$GROUPASSIGNED == data$GROUP
+
+
+a <- table(recid$Gender, recid$GROUPASSIGNED)
+b <- prop.table(a, 2)
